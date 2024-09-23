ed7a18ed2583ff49f70fe6ccb6faaba9278c0f08
Alpha-5002 <abishekravi2005@gmail.com>
2024-09-24 00:12:39 +0530

    Add files via upload

---
+++ Traffic_Light_Controller/Traffic_Light_LED.ino
@@ -0,0 +1,22 @@
+const int RED_led = 12;
+const int YEL_led = 10;
+const int GRE_led = 8;
+void setup()
+{
+  pinMode(RED_led, OUTPUT);
+  pinMode(YEL_led, OUTPUT);
+  pinMode(GRE_led, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(RED_led, HIGH);
+  delay(45000); 
+  digitalWrite(RED_led, LOW);
+  digitalWrite(YEL_led, HIGH);
+  delay(5000);
+  digitalWrite(YEL_led, LOW);
+  digitalWrite(GRE_led, HIGH);
+  delay(30000);
+  digitalWrite(GRE_led, LOW);
+}
